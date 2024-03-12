e3d133efe6d9c0b3799cff59bdbd2b3fa07c16d1
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-12 12:36:14 -0700

    Multiline html with

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   //templateUrl: './app.component.html',
-  template: '<h1>Hello World from inline template!</h1>',
+  template:  `<h1>Hello World from inline template!</h1>
+  <p>Angular is awesome</p>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
