4abde9ea2c5e4a0a146bb578d8de354262c0ec29
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-12 12:48:21 -0700

    component prefix changed from app to hinv (hotel inventory)

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './hinv.component.html',
   // template:  `<h1>Hello World from inline template!</h1>
   // <p>Angular is awesome</p>`,
   styleUrl: './app.component.css',
-  styles: [`h1{color:red}`]
+  //styles: [`h1{color:red}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
