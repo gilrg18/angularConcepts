7b5a97d4fcc44e4cb58fd75988ae1a94ce16d1a7
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-12 13:00:10 -0700

    Creating and rendering a new component 'rooms'

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
 
 @Component({
-  selector: 'hinv-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './hinv.component.html',
-  // template:  `<h1>Hello World from inline template!</h1>
-  // <p>Angular is awesome</p>`,
-  styleUrl: './app.component.css',
-  //styles: [`h1{color:red}`]
+    selector: 'hinv-root',
+    standalone: true,
+    templateUrl: './hinv.component.html',
+    // template:  `<h1>Hello World from inline template!</h1>
+    // <p>Angular is awesome</p>`,
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RoomsComponent]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
