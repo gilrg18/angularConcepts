2696eb4239d58793b1d4816a38eb873b48d12823
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-13 12:17:32 -0700

    Lifecycle HOOKS - ngOnInit

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
 import { CommonModule } from '@angular/common';
 
@@ -10,7 +10,8 @@ import { CommonModule } from '@angular/common';
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css',
 })
-export class RoomsComponent {
+
+export class RoomsComponent implements OnInit{
   //INTERPOLATION
   hotelName = 'RIU Hotel';
   //PROPERTY BINDING
@@ -25,38 +26,55 @@ export class RoomsComponent {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [{
-    roomNumber: 3,
-    roomType:'Deluxe',
-    amenities: 'Air conditioner, bar',
-    price: 500,
-    photos: 'asdasd.jpg',
-    checkinTime: new Date('11-Nov-2023'),
-    checkoutTime: new Date(),
-    rating : 8.2123,
-  },
-  {
-    roomNumber: 9,
-    roomType:'Normal',
-    amenities: 'Nothing lol',
-    price: 200,
-    photos: 'asdasd.jpg',
-    checkinTime: new Date('11-Nov-2023'),
-    checkoutTime: new Date(),
-    rating : 6.8321,
-  },
-  {
-    roomNumber:1,
-    roomType:'Pent House',
-    amenities: 'Air conditioner, bar, hottub',
-    price: 1000,
-    photos: 'asdasd.jpg',
-    checkinTime: new Date('11-Nov-2023'),
-    checkoutTime: new Date(),
-    rating : 10.0444,
-  }]
+  roomList: RoomList[] = [];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+  /**
+   * LIFECYCLE HOOKS
+   * ngOnChanges
+   * ngOnInit
+   * ngAfterContentInit
+   * ngAfterContentChecked
+   * ngAfterViewInit
+   * ngAfterViewChecked
+   * ngOnDestroy
+   */
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.roomList = [{
+      roomNumber: 3,
+      roomType:'Deluxe',
+      amenities: 'Air conditioner, bar',
+      price: 500,
+      photos: 'asdasd.jpg',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date(),
+      rating : 8.2123,
+    },
+    {
+      roomNumber: 9,
+      roomType:'Normal',
+      amenities: 'Nothing lol',
+      price: 200,
+      photos: 'asdasd.jpg',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date(),
+      rating : 6.8321,
+    },
+    {
+      roomNumber:1,
+      roomType:'Pent House',
+      amenities: 'Air conditioner, bar, hottub',
+      price: 1000,
+      photos: 'asdasd.jpg',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date(),
+      rating : 10.0444,
+    }]
+  
   }
 }
