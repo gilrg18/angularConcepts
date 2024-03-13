e4baf65b9e3e6ce22d1de9718195c866a59f5054
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-12 17:21:13 -0700

    ngSwitch and difference between structural and attribute directives

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'hinv-root',
@@ -9,8 +10,10 @@ import { RoomsComponent } from "./rooms/rooms.component";
     // template:  `<h1>Hello World from inline template!</h1>
     // <p>Angular is awesome</p>`,
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RoomsComponent]
+    imports: [RouterOutlet, RoomsComponent, CommonModule]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  loginType = 'Admin'
 }
