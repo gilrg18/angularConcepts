17828152905b33f3b8bbba001d7b43744f6454db
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-14 19:42:19 -0700

    Assigning a value to html element using ViewChild nativeElement

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
 import { CommonModule } from '@angular/common';
@@ -15,15 +15,21 @@ import { CommonModule } from '@angular/common';
 
 //when we use ViewChild, the static property is false, so we need to use AfterViewInit
 //so the parent renders before the child
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements OnInit{
   title = 'hotelinventoryapp';
 
   loginType = 'Admin'
 
-  @ViewChild('user',{ read: ViewContainerRef} ) vcr!: ViewContainerRef;
+  @ViewChild('name',{static: true}) name!: ElementRef;
 
-  ngAfterViewInit(): void {
-    const componentRef = this.vcr.createComponent(RoomsComponent)
-    componentRef.instance.numberOfRooms = 50;
+  ngOnInit() {
+    console.log('name: ',this.name.nativeElement.innerText = 'Gil Gil Gil')
   }
+
+  // @ViewChild('user',{ read: ViewContainerRef} ) vcr!: ViewContainerRef;
+
+  //ngAfterViewInit(): void {
+    // const componentRef = this.vcr.createComponent(RoomsComponent)
+    // componentRef.instance.numberOfRooms = 50;
+  //}
 }
