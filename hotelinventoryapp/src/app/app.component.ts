573e5ca17915ec883684d97424c4642b0a13ce14
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-26 15:37:54 -0700

    Registering and calling the localStorage value provider service

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Optional, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, Optional, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './container/container.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { LoggerService } from './logger/logger.service';
+import { LocalStorageToken } from './localstorage.token';
 
 @Component({
     selector: 'hinv-root',
@@ -27,11 +28,16 @@ export class AppComponent implements OnInit{
 
 
   //@Optional - Instead of throwing an error, it returns null if the service is not found.
-  constructor(@Optional() private loggerService: LoggerService){}
+  constructor(@Optional() private loggerService: LoggerService,
+  @Inject(LocalStorageToken) private localStorage: Storage){
+
+  }
 
   ngOnInit() {
     this.loggerService?.Log('LoggerService at AppComponent.ngOnInit()');
     console.log('name: ',this.name.nativeElement.innerText = 'Gil Gil Gil')
+    
+    this.localStorage.setItem('name', 'GRG');
   }
 
   // @ViewChild('user',{ read: ViewContainerRef} ) vcr!: ViewContainerRef;
